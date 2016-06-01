cf0cb16d2016f1e074b736569e3506b6e7a84621
leo.niu <junkainiu@gmail.com>
2016-06-01 19:17:13 +0800

    dd

--- src/index.py
+++ src/index.py
@@ -7,5 +7,14 @@ def handle_entry():
     return render_template("index.html")
 
 
+@app.route("/lihua")
+def handle_lihua():
+    return 'hello lihua'
+
+
+@app.route("/zkf")
+def handle_zkf():
+    return "hello zkf"
+
 if __name__ == "__main__":
     app.run()
